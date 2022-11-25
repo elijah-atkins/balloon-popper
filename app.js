f22fe4db6798c7b034c8f618d17bca66fee51639
Elijah Atkins <elijahorama@gmail.com>
2022-11-24 17:54:55 -0700

    added game loop

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ class App {
     colors = [0xff0000, 0xff707b, 0x30ee30, 0x006400, 0x66bbe6, 0x0000ff, 0xffff00, 0xffa500, 0x800080, 0xff00ff];
     constructor() {
         const scorePanel = document.getElementById("score");
+        this.gameOver = true;
         this.score = 0;
+        this.lives = 0;
+
         //update the score
+
         scorePanel.innerHTML = "Score: " + this.score;
         const container = document.createElement('div');
         document.body.appendChild(container);
@@ -19,6 +23,7 @@ class App {
         this.camera.lookAt(-6, 4, 0);
         this.timer = new THREE.Clock();
         this.scene = new THREE.Scene();
+
 
 
         const ambient = new THREE.HemisphereLight(0xffffff, 0x666666, 0.3);
@@ -46,27 +51,81 @@ class App {
 
         window.addEventListener('click', this.onClick.bind(this));
         window.addEventListener('resize', this.resize.bind(this));
-
+        //event listener for the start button
+        const startButton = document.getElementById("start");
+        startButton.addEventListener('click', this.newGame.bind(this));
 
     }
+    //hide all balloons
+    hideAll() {
+        for (let i = 0; i < this.scene.children.length; i++) {
+            this.scene.children[i].visible = false;
+        }
+        //show the start button
+        const startButton = document.getElementById("start");
+        startButton.style.visibility = "visible";
+    }
+
+    //show all
+    showAll() {
+        for (let i = 0; i < this.scene.children.length; i++) {
+            this.scene.children[i].visible = true;
+            //subtract 10 from y position if the balloon is over 5
+            if (this.scene.children[i].position.y > 0) {
+                this.scene.children[i].position.y -= 25;
+            }
+        }
+        //hide the start button
+        const startButton = document.getElementById("start");
+        startButton.style.visibility = "hidden";
+    }
+
+    newGame() {
+        this.gameOver = false;
+        this.setLives(5);
+        this.setScore(0);
+        this.showAll();
+    }
+
+    setScore(s) {
+        this.score = s;
+        const scorePanel = document.getElementById("score");
+        scorePanel.innerHTML = "Score: " + this.score;
+    }
+
+    setLives(l) {
+        this.lives = l;
+        if (this.lives === 0) {
+            this.hideAll();
+            this.gameOver = true;
+
+        }
+        const livesPanel = document.getElementById("lives");
+        livesPanel.innerHTML = "Lives: " + this.lives;
+    }
+
 
     onClick(event) {
-        //get the mouse position
-        const mouse = new THREE.Vector2();
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-        //create a raycaster
-        const raycaster = new THREE.Raycaster();
-        raycaster.setFromCamera(mouse, this.camera);
-        //get the objects that the raycaster intersects
-        const intersects = raycaster.intersectObjects(this.scene.children, true);
-        //if there is an intersection, reset the balloon position
-        if (intersects.length > 0) {
-            this.resetBalloon(intersects[0].object.parent);
-            this.score += 1;
-            const scorePanel = document.getElementById("score");
-            scorePanel.innerHTML = "Score: " + this.score;
+        if (!this.gameOver) {
+            //get the mouse position
+            const mouse = new THREE.Vector2();
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+            //create a raycaster
+            const raycaster = new THREE.Raycaster();
+            raycaster.setFromCamera(mouse, this.camera);
+            //get the objects that the raycaster intersects
+            const intersects = raycaster.intersectObjects(this.scene.children, true);
+            //if there is an intersection, reset the balloon position
+            if (intersects.length > 0) {
+                this.resetBalloon(intersects[0].object.parent);
+                this.score += 1;
+                this.setScore(this.score);
+                //console log the position of the balloon
+          //      console.log(intersects[0].object.parent.position);
+            }
         }
+        
     }
 
 
@@ -93,7 +152,7 @@ class App {
     loadGLTF() {
         //new random color
         const color = this.colors[Math.floor(Math.random() * this.colors.length)];
-        // new material
+        // new material with random color
         const material = new THREE.MeshStandardMaterial({
             color: color,
             roughness: 0.1,
@@ -139,7 +198,7 @@ class App {
         let frame = this.timer.getElapsedTime();
         const index = this.scene.children.indexOf(this.balloon);
         const color = this.colors[Math.floor(Math.random() * this.colors.length)];
-        balloon.position.set(Math.sin(index + frame * 5) * 10 - 5, -Math.sin(index + frame * 10) * 10 - 15, 0);
+        balloon.position.set(Math.sin(index + frame * 5) * 10 - 5, -Math.sin(index + frame * 10) * 10 - 25, 0);
         const s = Math.random() * 0.25 + 0.75;
         balloon.scale.set(s, s, s);
         //set the balloon color
@@ -164,38 +223,43 @@ class App {
 
     render() {
         //find every child of the scene and rotate it
-        this.scene.children.forEach((child) => {
-            
-            child.position.y += 0.05;
-            child.rotation.y += Math.sin(child.position.y * 1) * 0.01;
-            child.rotation.z += .01;
-            if (child.position.y > 20) {
-                //reset the balloon
-                this.resetBalloon(child);
-            }
-            //check for collision
-            if (this.checkCollision(child)) {
-                //bump the balloon
-                this.bumpBalloon(child);
-            }
+        if (!this.gameOver) {
+            this.scene.children.forEach((child) => {
+                child.position.y += 0.05;
+                child.rotation.y += Math.sin(child.position.y * 1) * 0.01;
+                child.rotation.z += .01;
+                if (child.position.y > 20) {
+                    //reset the balloon
 
-        });
+                    this.resetBalloon(child);
+                    //if the position.x in in view, subtract 1 from the score
+                    if (child.position.x > -3 && child.position.x < 3) {
+                        //if the balloon is visible, subtract 1 from the score
+                        if (child.visible) {
+                            this.lives -= 1;
+                            this.setLives(this.lives);
+                        }
+                    }
+                }
+                //check for collision
+                if (this.checkCollision(child)) {
+                    //bump the balloon
+                    this.bumpBalloon(child);
+                }
+
+            });
 
 
-        this.renderer.render(this.scene, this.camera);
+            this.renderer.render(this.scene, this.camera);
+        }
     }
     //check for collision
 
     checkCollision(balloon) {
-        //get current balloon index
         const index = this.scene.children.indexOf(balloon);
-        //loop through all the balloons
         for (let i = 0; i < this.scene.children.length; i++) {
-            //check if the balloon is visible
             if (this.scene.children[i].visible) {
-                //check if the balloon is not the current balloon
                 if (i != index) {
-                    //check if the distance between the current balloon and the other balloon is less than 1
                     if (this.scene.children[i].position.distanceTo(balloon.position) < 2) {
                         //return true if there is a collision
                         return true;
